4fa16977192af9f80feb5bd58d5be7c5e9c3bad4
Hyperival <sanjaygautam779@gmail.com>
2022-11-13 12:01:27 +0530

    Adding uc2 code

--- EmployeeWageCompute/EmployeeWage.cs
+++ EmployeeWageCompute/EmployeeWage.cs
@@ -13,15 +13,33 @@ namespace EmployeeWageCompute
             //main
             Console.WriteLine("Welcome to EmployeeWage Computation");
             //UC1 
-            int employeePresent = 1;
+            Console.WriteLine("Welcome to EmployeeWage Computation");
+            //UC1 
+            const int EMP_RATE_PER_HR = 20;
+            const int FULL_TIME = 1;
+            const int PART_TIME = 2;
+            int empWage = 0;
+            int empHrs = 0;
+
             Random r = new Random();
-            int employeeInput = r.Next(0, 2);
-
-            if (employeePresent == employeeInput)
-                Console.WriteLine("Employee is Present");
-            else
-                Console.WriteLine("Employee is Absent");
-
+            int employeeInput = r.Next(0, 3);
+            switch (employeeInput)
+            {
+                case FULL_TIME:
+                    Console.WriteLine("FullTime Employee is Present");
+                    empHrs = 8;
+                    break;
+                case PART_TIME:
+                    Console.WriteLine("PartTime Employee is Present");
+                    empHrs = 4;
+                    break;
+                default:
+                    Console.WriteLine("Employee is Absent");
+                    break;
+            }
+            //UC2
+            empWage = EMP_RATE_PER_HR * empHrs;
+            Console.WriteLine("Daily Employee Wage is:" + empWage);
             Console.ReadLine();
 
         }
