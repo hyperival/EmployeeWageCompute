bd8a590cb5b98d8393c063be299e5b4537d820f7
Hyperival <sanjaygautam779@gmail.com>
2022-11-13 12:12:19 +0530

    Adding uc5 code to EmployeeWageuc5 branch

--- EmployeeWageCompute/EmployeeWage.cs
+++ EmployeeWageCompute/EmployeeWage.cs
@@ -15,28 +15,32 @@ namespace EmployeeWageCompute
             const int IS_PART_TIME = 1;
             const int IS_FULL_TIME = 2;
             const int EMP_RATE_PER_HOUR = 20;
-            int empHrs = 0;
-            int empWage = 0;
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            const int NUM_OF_WORKING_DAYS = 2;
+            int empHrs = 0, empWage = 0, totalEmpWage = 0;
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
             {
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME:
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
 
-                    Console.WriteLine("Employee is present");
-                    empHrs = 8;
-                    break;
-                default:
-                    Console.WriteLine("Employee is absent");
-                    empHrs = 0;
-                    break;
+                        Console.WriteLine("Employee is present");
+                        empHrs = 8;
+                        break;
+                    default:
+                        Console.WriteLine("Employee is absent");
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("EmpWage :" + empWage);
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("EmpWage :" + empWage);
-
+            Console.WriteLine("Total Emp Wage :" + totalEmpWage);
         }
     }
 }
