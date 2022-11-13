18c01925043f5f0051324f2dbb2c0c696b0ee83e
Hyperival <sanjaygautam779@gmail.com>
2022-11-13 11:55:24 +0530

    Adding welcome message to main

---
+++ EmployeeWageCompute/EmployeeWage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageCompute
+{
+    internal class EmployeeWage
+    {
+        static void Main(string[] args)
+        {
+            //main
+            Console.WriteLine("Welcome to EmployeeWage Computation");
+            
+        }
+    }
+}
